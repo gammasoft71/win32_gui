2a03b5ad7432b69638aa70b044e94a270af99e23
Gammasoft <gammasoft71@gmail.com>
2022-09-15 22:51:41 +0200

    Update application.h

--- win32_gui/src/win32_gui/include/win32/forms/application.h
+++ win32_gui/src/win32_gui/include/win32/forms/application.h
@@ -62,8 +62,13 @@ namespace win32 {
       static void do_events();
 
       /// @brief Enables dark mode for the application.
+      /// @remarks By default, the dark mode is enabled automatically if the system is in dark mode.
+      /// @remarks xtd::forms::application::enable_dark_mode sets the application in dark mode even if your system is in light mode.
       static void enable_dark_mode();
+
       /// @brief Enables light mode for the application.
+      /// @remarks By default, the light mode is enabled automatically if the system is in light mode.
+      /// @remarks xtd::forms::application::enable_light_mode sets the application in light mode even if your system is in dark mode.
       static void enable_light_mode();
 
       /// @brief Informs all message pumps that they must terminate, and then closes all application windows after the messages have been processed.
